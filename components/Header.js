38c2a898eac0965237413d68369a169a8c943547
shlok <shlokjjw@gmail.com>
2021-12-19 13:33:04 +0530

    chat functionality complete

---
+++ components/Header.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { useMoralis } from "react-moralis";
+import Avatar from "./Avatar";
+import ChangeUsername from "./ChangeUsername";
+
+function Header() {
+  const { user } = useMoralis();
+
+  return (
+    <div className="sticky top-0 p-5 z-50 bg-black shadow-sm border-b-2 border-pink-700 text-pink-500">
+      <div className="grid grid-col-5 lg:grid-col-6 items-end lg:items-center">
+        <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+            src="https://links.papareact.com/3pi"
+          />
+        </div>
+        <div className="col-span-4 text-left lg:text-center">
+          <div className="relative h-48 w-48 lg:mx-auto border-pink-500 border-8 rounded-full">
+            <Avatar logoutOnPress />
+          </div>
+
+          <h1 className="text-3xl">Welcome to Shlok's community</h1>
+
+          <h2 className="text-5xl font-bold truncate">{user.getUsername()}</h2>
+
+          <ChangeUsername />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
